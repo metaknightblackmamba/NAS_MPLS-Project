28ccee364e6484f21913bc738b77857c69381bfe
Marc CHAPUS <marc.chapus@twinswheel.fr>
2021-01-08 09:51:07 +0100

    Add network config generator

---
+++ netgen.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+
+let file = fs.readFileSync("test.json")
+let data = JSON.parse(file)
+
+//console.log(data.version)
+
+routers = []
+
+// Add routers to routers array
+for(let k in data){
+  if(k != "version" ){
+    routers.push(k)
+  }
+}
+
+console.log(routers.length + " routers found !")
+
+for (let i = 0; i < routers.length; i++) {
+  console.log(data[routers[i]])
+}
